233929403ccde9c50cbaeca3dee608af9ed52c40
Uriah Finkel <ufinkel@gmail.com>
2025-10-16 08:21:26 +0300

    feat: introduce calculate_pseudostates

--- src/pseudostate/pseudo.py
+++ src/pseudostate/pseudo.py
@@ -1,7 +1,23 @@
 import polars as pl
+from polarstate import prepare_event_table, predict_aj_estimates
 
 
 def calculate_pseudostates(
     times_and_reals: pl.DataFrame, fixed_time_horizon: int
 ) -> pl.DataFrame:
-    pass
+    pseudo_states: list[pl.DataFrame] = []
+
+    for i in range(times_and_reals.height):
+        sub_data = times_and_reals.slice(0, i).vstack(
+            times_and_reals.slice(i + 1, times_and_reals.height - i)
+        )
+
+        sub_event_table = prepare_event_table(sub_data)
+
+        pseudo_state = predict_aj_estimates(
+            sub_event_table, fixed_time_horizons=pl.Series([fixed_time_horizon])
+        )
+
+        pseudo_states.append(pseudo_state)
+
+    return pl.concat(pseudo_states, how="vertical")
